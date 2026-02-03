525dab5e7e1a4cb7870df6132f951c6bf807ffe2
rcottinet <romancottinet@gmail.com>
2026-02-03 14:39:06 +0100

    update url

--- start/env.ts
+++ start/env.ts
@@ -24,4 +24,6 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
   SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
+
+  URL: Env.schema.string({ format: 'url' }),
 })
